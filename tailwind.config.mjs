e81c86e9620926eaa68725573627c79124844129
Alwan7 <alilewli0@gmail.com>
2025-01-28 23:02:14 +0100

    feat: Enhance Tailwind CSS configuration with new theme colors, gradients, and font families

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,44 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+
+const config = {
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            colors: {
+                neuroBlue: "#1E88E5",
+                neuroBlueLight: "#BBDEFB",
+                neuroGreen: "#00C853",
+                neuroGreenLight: "#B9F6CA",
+                neuroPink: "#E91E63",
+                neuroPinkLight: "#F8BBD0",
+                neuroGold: "#FFC107",
+                neuroGoldLight: "#FFECB3",
+                neuroGray: "#ECEFF1",
+                neuroDarkGray: "#263238",
+                neuroPurple: "#7C4DFF",
+                neuroPurpleLight: "#D1C4E9",
+                backgroundDark: "#121212",
+                backgroundLight: "#FAFAFA",
+            },
+            boxShadow: {
+                fancy: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
+                glowing: "0 0 10px rgba(30, 136, 229, 0.8)",
+            },
+            fontFamily: {
+                fancy: ['"Poppins"', "sans-serif"],
+                mono: ['"Roboto Mono"', "monospace"],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
+
+export default config;
