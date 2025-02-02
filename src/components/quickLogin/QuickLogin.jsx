32e6ef50dd2a4787ea36339abe7b01c7c3b5d20a
Alwan7 <alilewli0@gmail.com>
2025-02-02 22:23:18 +0100

    refactor: Convert QuickLogin component to use hooks for API calls

--- src/components/quickLogin/QuickLogin.jsx
+++ src/components/quickLogin/QuickLogin.jsx
@@ -1,35 +1,43 @@
+"use client";
+import React, { useEffect } from 'react';
 import config from '@/config/config';
 import { v4 as uuidv4 } from 'uuid';
-import { fetchServiceId } from '@/utils/quickLogin';
-import { fetchQrCode } from '@/utils/quickLogin';
+import { fetchServiceId, fetchQrCode } from '@/utils/quickLogin';
 import QrCode from '@/components/quickLogin/QrCode';
 
-export default async function QuickLogin() {
-
+export default function QuickLogin() {
   const service = config.quickLogin.callBackUrl;
+  console.log(service);
+  console.log(config);
 
-  if(!service.startsWith('https://')){
+  if (!service.startsWith('https://')) {
     console.error('Misconfigured callback URL: The URL must use HTTPS.');
   }
 
   const tabId = uuidv4().replaceAll('-', '');
   const sessionId = uuidv4().replaceAll('-', '');
-  let serviceId, qrCodeUrl;
 
-  try {
-    serviceId = await fetchServiceId({ service, sessionId });
-    qrCodeUrl = await fetchQrCode({ serviceId, tabId });
-  } catch(error) {
-    console.error('Error fetching QR code data', error);
-    return null;
+  // Call API in `useEffect` (Client-side)
+  const [serviceId, setServiceId] = React.useState(null);
+  const [qrCodeUrl, setQrCodeUrl] = React.useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const serviceId = await fetchServiceId({ service, sessionId });
+        const qrCodeUrl = await fetchQrCode({ serviceId, tabId });
+        setServiceId(serviceId);
+        setQrCodeUrl(qrCodeUrl);
+      } catch (error) {
+        console.error('Error fetching QR code data', error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (!serviceId || !qrCodeUrl) {
+    return <p>Loading QR Code...</p>;
   }
 
-  return (
-    <QrCode
-      initialQrCodeUrl={qrCodeUrl}
-      serviceId={serviceId}
-      sessionId={sessionId}
-      tabId={tabId}
-    />
-  );
+  return <QrCode initialQrCodeUrl={qrCodeUrl} serviceId={serviceId} sessionId={sessionId} tabId={tabId} />;
 }
