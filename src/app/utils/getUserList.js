6a54a5f87ab6728ba65a8bd6227f5a9cfca5f4fc
Vincent <vincentmu3@hotmail.com>
2025-02-11 09:19:24 +0100

    Server Component Test 2

--- src/app/utils/getUserList.js
+++ src/app/utils/getUserList.js
@@ -1,7 +1,7 @@
 'use server'
 export async function getUserList(page = 1, limit = 5,query) {
     // Fetch mockdata with pagination values
-    const res = await fetch(`${process.env.URL}/api/mockdata?page=${page}&limit=${limit}&query=${encodeURIComponent(query)}`, {
+    const res = await fetch(`https://neuro-admin-dev-gnega5g4acbpfddm.germanywestcentral-01.azurewebsites.net/api/mockdata?page=${page}&limit=${limit}&query=${encodeURIComponent(query)}`, {
     })
 
     if (!res.ok) {
