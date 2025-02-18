da57017d0c28c7068a78e8c72b96a9e4f458d244
N1N3T4L3S <6765614+ninetales@users.noreply.github.com>
2025-02-18 14:31:30 +0100

    refactor(LegalIdButton): update endpoint path

--- src/components/legalIdButton/LegalIdButton.jsx
+++ src/components/legalIdButton/LegalIdButton.jsx
@@ -1,27 +1,29 @@
+import config from '@/config/config';
 export default function LegalIdButton() {
+  const handleFetch = async () => {
+    try {
+      const { protocol, origin } = config;
+      const url = `${protocol}://${origin}/api/mockdata/legalIdentities`;
 
-    const handleFetch = async () => {
-        console.log('Fetching legal identities');
-        try {
-            const response = await fetch(`http://localhost:3000/api/mockdata/legalIdentities`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include'
-            });
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
 
-            if(!response.ok){
-                console.error('Response not ok ', response.statusText);
-            }
+      if (!response.ok) {
+        console.error('Response not ok ', response.statusText);
+      }
 
-            const responseData = await response.json();
+      const responseData = await response.json();
 
-            console.log('Response Data:', responseData);
-        } catch (err) {
-            console.error('Fetch error', err);
-        }
+      console.log('Response Data:', responseData);
+    } catch (err) {
+      console.error('Fetch error', err);
     }
+  };
 
-    return <button onClick={() => handleFetch()}>Fetch legal identities</button>
+  return <button onClick={() => handleFetch()}>Fetch legal identities</button>;
 }
