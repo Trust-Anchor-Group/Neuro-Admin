d19ff17603f62ab927c97c1bb99f4b62eedda155
Alwan7 <alilewli0@gmail.com>
2025-03-02 15:14:45 +0100

    fix(fetchOrders): enhance API request with cookie handling and improve error message

--- src/lib/fetchOrders.js
+++ src/lib/fetchOrders.js
@@ -1,19 +1,26 @@
+import { cookies } from "next/headers"; 
 export async function fetchOrders() {
   try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_ORIGIN}/api/getTokens`, {
+    const clientCookies = await cookies(); 
+
+      const url = `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_ORIGIN}/api/tokens`;
+
+    const response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Cookie": clientCookies.toString(), 
       },
       body: JSON.stringify({
         maxCount: 100, 
         offset: 0,
       }),
-      cache: "no-store", 
+      cache: "no-store",
+      credentials: "include", 
     });
 
     if (!response.ok) {
-      throw new Error(`Error fetching orders: ${response.statusText}`);
+      throw new Error(`Error fetching orders: ${response.status} - ${response.statusText}`);
     }
 
     const data = await response.json();
@@ -21,9 +28,10 @@ export async function fetchOrders() {
     const formattedOrders = data.data.map((token) => ({
       id: token.tokenId, 
       category: token.category,
+      assetName: token.friendlyName,
       orderDate: token.createdDate,
       amount: `${token.value} ${token.currency}`,
-      status: "pending", 
+      status: "pending",
     }));
 
     return { orders: formattedOrders, loading: false };
