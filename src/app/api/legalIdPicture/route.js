f0eed285575f1adf905890cc765f173a7cc9c4fb
Eliot <eliotcolegate@gmail.com>
2025-03-10 13:57:51 +0100

    feat: get legal Id image

---
+++ src/app/api/legalIdPicture/route.js
@@ -0,0 +1,66 @@
+import config from "@/config/config";
+import ResponseModel from "@/models/ResponseModel";
+
+export async function POST(request) {
+
+    const requestData = await request.json();
+    const { legalId } = requestData;
+    let dimension;
+    if (requestData.dimension > 120 || !requestData.dimension) {
+        dimension = 120
+    } else {
+        dimension = requestData.dimension;
+    }
+
+    const { host } = config.api.agent;
+    const url = `https://${host}/QuickLogin/Session/${host}/Attachments/${legalId}?Width=${dimension}&Height=${dimension}`;
+
+    console.log(url)
+
+    try {
+
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+            }
+        });
+
+        const contentType = response.headers.get('content-type');
+        let data;
+
+        if (contentType.includes('image/webp')) {
+            data = await response.blob();
+        } else {
+            data = await response.text();
+        }
+
+        if (!response.ok) {
+            return new Response(JSON.stringify(new ResponseModel(response.status, `Error: ${data}`)), {
+                status: response.status,
+                headers: {
+                    'Content-Type': 'image/webp'
+                }
+            });
+        }
+
+        return new Response(data, {
+            status: 200,
+            headers: {
+                'Content-Type': 'image/webp'
+            }
+        });
+
+
+    } catch (error) {
+        const statusCode = error.statusCode || 500;
+        const message = error.message || 'Internal Server Error';
+        return new Response(JSON.stringify(new ResponseModel(statusCode, message)), {
+            status: statusCode,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        );
+    }
+}
