2c785afe25279a68d9b9e1171a928a0aa90172a4
Vincent <vincentmu3@hotmail.com>
2025-05-13 14:50:03 +0200

    Modal update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -36,7 +36,8 @@ const config = {
                 neuroOrange:"#F18334",
                 neuroDarkOrange:"#A84700",
                 neuroRed:"#F2495C",
-                neuroButtonGray:"#DFE1E3"
+                neuroButtonGray:"#DFE1E3",
+                neuroInputBackground:"#F5F6F7"
             },
             boxShadow: {
                 fancy: "0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)",
