ae31f6b7534446e412fc50b86c37ee5d2f34634e
Alwan7 <alilewli0@gmail.com>
2025-06-27 14:07:28 +0200

    refactor(InfoToggleButton) Swap icon order for toggle button for improved UX

--- src/components/access/Buttons/InfoToggleButton.jsx
+++ src/components/access/Buttons/InfoToggleButton.jsx
@@ -8,8 +8,8 @@ export const InfoToggleButton = ({infoToggle,setIntoToggle,title}) => {
           (
           <div className={`flex justify-between ${infoToggle === true ? 'border-b-2 pb-4' :''}`} >
             <h2 className='font-semibold text-neuroDarkGray/70'>{title}</h2>
-            <button onClick={() => setIntoToggle(prev => !prev)}>{infoToggle ? <FaChevronDown color='#6e6e6e' /> 
-            : <FaChevronUp color='#6e6e6e' />}</button>
+            <button onClick={() => setIntoToggle(prev => !prev)}>{infoToggle ? <FaChevronUp color='#6e6e6e' /> 
+              : <FaChevronDown color='#6e6e6e' />}</button>
              </div>)
           }
     </div>
