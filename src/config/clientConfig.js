a0d9a3be621649dabc746f76ad6b223eb02f5447
Alwan7 <alilewli0@gmail.com>
2025-06-30 15:44:16 +0200

    feat(clientConfig) Add client configuration for branding management

---
+++ src/config/clientConfig.js
@@ -0,0 +1,24 @@
+// src/config/clientConfig.js
+export const clientConfigs = {
+  kikkin: {
+    brand: {
+      primaryColor: '#2f8aff',
+      secondaryColor: '#FFB900',
+      backgroundColor: '#005375',
+      accentColor: '#FBB040',
+      logoUrl: '/kikkinLogo.jpg',
+    },
+ 
+  
+  },
+  Mateo: {
+    brand: {
+      primaryColor: '#7C4DFF',
+      secondaryColor: '#FFC107',
+      backgroundColor: '#FFFFFF',
+      accentColor: '#7C4DFF',
+      logoUrl: '/NeuroLogo.svg',
+    },
+    
+  },
+}
