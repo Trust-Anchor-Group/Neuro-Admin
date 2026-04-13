a32f1d4352d7fa37657e7eef80e45c79e26c8370
Alwan7 <alilewli0@gmail.com>
2026-04-13 09:26:14 +0200

    chore: update dependencies and linting configuration
    - Changed linting command from "next lint" to "eslint ."
    - Updated lodash from version 4.17.23 to 4.18.1
    - Upgraded next from version 16.1.7 to 16.2.3
    - Updated eslint-config-next from version 15.1.4 to ^16.2.3

--- next.config.mjs
+++ next.config.mjs
@@ -1,48 +1,31 @@
 /** @type {import('next').NextConfig} */
-const backendHost = process.env.NEXT_PUBLIC_AGENT_HOST || process.env.AGENT_HOST || 'mateo.lab.tagroot.io';
+const backendHost =
+  process.env.NEXT_PUBLIC_AGENT_HOST ||
+  process.env.AGENT_HOST ||
+  "mateo.lab.tagroot.io";
 
 const nextConfig = {
-
   async redirects() {
     return [
       {
-        source: '/',
-        destination: '/login',
+        source: "/",
+        destination: "/login",
         permanent: true,
       },
     ];
   },
   reactStrictMode: true,
-   output: 'standalone',
-    env: {
-    NEXT_PUBLIC_PROTOCOL: process.env.NEXT_PUBLIC_PROTOCOL,
-    NEXT_PUBLIC_ORIGIN: process.env.NEXT_PUBLIC_ORIGIN,
-    NEXT_PUBLIC_AGENT_HOST: process.env.NEXT_PUBLIC_AGENT_HOST,
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
-  },
- images: {
-   dangerouslyAllowSVG: true,
+  output: "standalone",
+  images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: "attachment",
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-      },
-      {
-        protocol: 'https',
-        hostname: backendHost,
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "api.dicebear.com" },
+      { protocol: "https", hostname: backendHost },
     ],
   },
 };
